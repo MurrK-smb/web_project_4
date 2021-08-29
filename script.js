0250aedba1afd2597df5ec807a7191843b974570
NicolasGil <nick.gil360@gmail.com>
2021-08-29 05:11:36 -0400

    attempt 2

--- script.js
+++ script.js
@@ -3,42 +3,29 @@ const editBtn = document.querySelector('.info__edit-button')
 const modal = document.querySelector('.modal')
 const closeBtn = document.querySelector('.modal__close-button')
 
-function toggleModal() {
-  modal.classList.toggle('modal_open')
+function openModal() {
+  modal.classList.add('modal_open')
+  formName.value = infoName.textContent
+  formCaption.value = infoCaption.textContent
 }
 
-editBtn.addEventListener('click', toggleModal, false)
-closeBtn.addEventListener('click', toggleModal, false)
+function closeModal() {
+  modal.classList.remove('modal_open')
+}
 
-const formName = document.querySelector('.form__name')
-const formCaption = document.querySelector('.form__caption')
+const formName = document.querySelector('#form-name')
+const formCaption = document.querySelector('#form-caption')
 const infoName = document.querySelector('.info__name')
 const infoCaption = document.querySelector('.info__caption')
+const submit = document.querySelector('.form')
 
-formName.value = infoName.textContent
-formCaption.value = infoCaption.textContent
-
-const submit = document.querySelector('.form__submit')
-
-function setInfo() {
+function setInfo(e) {
   infoName.textContent = formName.value
   infoCaption.textContent = formCaption.value
-  toggleModal()
+  closeModal()
+  e.preventDefault()
 }
 
-submit.addEventListener('click', setInfo, false)
-
-const cards = document.querySelector('.cards')
-
-function like(e) {
-  if(e.target.classList.contains('card__like-image')) {
-    if(e.target.getAttribute('src') === 'images/heart.svg') {
-      e.target.setAttribute('src', 'images/heart_filled.svg')
-    }
-    else {
-      e.target.setAttribute('src', 'images/heart.svg')
-    }
-  }
-}
-
-cards.addEventListener('click', like, false)
+editBtn.addEventListener('click', openModal, false)
+closeBtn.addEventListener('click', closeModal, false)
+submit.addEventListener('submit', setInfo, false)
