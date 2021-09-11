7db8d814da08d23e17884349793448abe7e41e56
NicolasGil <nick.gil360@gmail.com>
2021-09-11 16:53:04 -0400

    refactor

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const closeBtns = Array.from(document.querySelectorAll('.modal__close-button'))
 const editModal = document.querySelector('#edit')
 const addModal = document.querySelector('#add')
 const imgModal = document.querySelector('#img')
+const imgModalCon = imgModal.querySelector('.modal__img-content')
 const infoName = document.querySelector('.info__name')
 const infoCaption = document.querySelector('.info__caption')
 const formName = document.querySelector('#form-name')
@@ -23,29 +24,30 @@ const cards = [
 const cardColl = document.querySelector('.cards')
 
 function openModal(modal) {
-  modal.classList.remove('modal_closed')
+  modal.classList.add('modal_open')
 }
 
-function closeModal(e) {
-  const modal = e.target.closest('.modal')
+function closeModal(modal) {
   modal.classList.add('fade-out')
   setTimeout(function() {
-    modal.classList.add('modal_closed')
+    modal.classList.remove('modal_open')
     modal.classList.remove('fade-out')
   }, 250)
 }
 
 function createCard(cardData) {
   const card = cardTemplate.querySelector('.card').cloneNode(true)
-  card.querySelector('.card__image').setAttribute('src', cardData.link)
+  const cardImage = card.querySelector('.card__image')
+  
+  cardImage.setAttribute('src', cardData.link)
   card.querySelector('.card__title').textContent = cardData.name
   
   card.querySelector('.card__like-button').addEventListener('click', function(e) {e.target.classList.toggle('card__like-button_a')})
   card.querySelector('.card__delete').addEventListener('click', function(e) {e.target.closest('.card').remove()})
-  card.querySelector('.card__image').addEventListener('click', function() {
+  cardImage.addEventListener('click', function() {
     openModal(imgModal)
-    imgModal.querySelector('.modal__img-content').setAttribute('src', cardData.link)
-    imgModal.querySelector('.modal__img-content').setAttribute('alt', cardData.link)
+    imgModalCon.setAttribute('src', cardData.link)
+    imgModalCon.setAttribute('alt', cardData.name)
     imgModal.querySelector('.modal__img-caption').textContent = cardData.name
   })
   return card
@@ -55,24 +57,26 @@ cards.forEach(function(cardData) {
   cardColl.append(createCard(cardData))
 })
 
-formName.value = infoName.textContent
-formCaption.value = infoCaption.textContent
-
-editBtn.addEventListener('click', function() {openModal(editModal)})
+editBtn.addEventListener('click', function() {
+  openModal(editModal)
+  formName.value = infoName.textContent
+  formCaption.value = infoCaption.textContent
+})
 addBtn.addEventListener('click', function() {openModal(addModal)})
 closeBtns.forEach(function(closeBtn) {
-  closeBtn.addEventListener('click', closeModal)
+  closeBtn.addEventListener('click', function() {
+    closeModal(this.closest('.modal'))
+  })
 })
 editModal.querySelector('.form').addEventListener('submit', function(e) {
   e.preventDefault()
   infoName.textContent = formName.value
   infoCaption.textContent = formCaption.value
-  closeModal(e)
+  closeModal(editModal)
 })
 addModal.querySelector('.form').addEventListener('submit', function(e) {
   e.preventDefault()
   const newCard = {name: formTitle.value, link: formLink.value}
-  cards.push(newCard)
   cardColl.prepend(createCard(newCard))
-  closeModal(e)
+  closeModal(addModal)
 })
